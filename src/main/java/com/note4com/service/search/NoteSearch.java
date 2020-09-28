7164e04f2c58e032d88a7f30f60d10db0dfed5e5
Dong Phan <dongphan@tmon.co.kr>
2020-09-28 18:57:53 +0700

    Define search function

--- src/main/java/com/note4com/service/search/NoteSearch.java
+++ src/main/java/com/note4com/service/search/NoteSearch.java
@@ -1,5 +1,9 @@
 package com.note4com.service.search;
 
-public class NoteSearch {
+import java.util.List;
 
+import com.note4com.model.Notes;
+
+public interface  NoteSearch {
+	List<Notes> search(String username, String keyword);
 }
---
+++ src/main/java/com/note4com/service/search/NoteSearchImpl.java
@@ -0,0 +1,15 @@
+package com.note4com.service.search;
+
+import java.util.List;
+
+import com.note4com.model.Notes;
+
+public class NoteSearchImpl implements NoteSearch {
+
+	@Override
+	public List<Notes> search(String username, String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
