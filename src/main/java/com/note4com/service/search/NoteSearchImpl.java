d18f13f5cf53f6726d7afc89c7561fde8f94bfc4
Dong Phan <dongphan@tmon.co.kr>
2020-09-29 11:16:14 +0700

    Update code

--- src/main/java/com/note4com/service/search/NoteSearchImpl.java
+++ src/main/java/com/note4com/service/search/NoteSearchImpl.java
@@ -3,15 +3,23 @@ package com.note4com.service.search;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.note4com.model.Notes;
+import com.note4com.repository.NotesRepository;
 import com.note4com.service.dto.NotesSearchDto;
 
 public class NoteSearchImpl implements NoteSearch {
-
+	@Autowired
+	NotesRepository noteRepository;
 	@Override
 	public List<Notes> search(NotesSearchDto notesSearchDto) {
 		// TODO Auto-generated method stub
-		return new ArrayList<Notes>();
+		List<Notes> result =  noteRepository.findAll();
+		if(result == null) {
+			result = new ArrayList<>();
+		}
+		return result;
 	}
 
 }
