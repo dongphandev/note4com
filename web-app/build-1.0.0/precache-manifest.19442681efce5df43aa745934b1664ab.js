94db3c5f381b811b5562010edd172ca3bf2ffebb
Truc Duong <dttruc7@gmail.com>
2020-10-09 22:45:34 +0700

    Build version 1.0.0

---
+++ web-app/build-1.0.0/precache-manifest.19442681efce5df43aa745934b1664ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ed2257bd49e8465095e90b9998b9cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "719297cd15520c2d4dd8",
+    "url": "/static/css/main.fd951dd3.chunk.css"
+  },
+  {
+    "revision": "e4f684a39e403a48d15a",
+    "url": "/static/js/2.bd607651.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd607651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719297cd15520c2d4dd8",
+    "url": "/static/js/main.91d2c33d.chunk.js"
+  },
+  {
+    "revision": "c4cda4214c0c300269f1",
+    "url": "/static/js/runtime-main.d5bfd598.js"
+  }
+]);
